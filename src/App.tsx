963fdc2ac84f618f747d2ca4ecee4e7b3b8bbd9e
Alessandro Juárez <eajuarezh@gyt.com.gt>
2023-03-02 21:11:53 -0600

    ADD: Commit inicial de la aplicación.

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <AppBar position="fixed">
+          <Toolbar variant='dense'>
+            <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" component="div">
+              Cuentas por Pagar
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Toolbar />
+      </div>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button style={{ backgroundColor: "blue" }} onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+
+
+
+export default App
